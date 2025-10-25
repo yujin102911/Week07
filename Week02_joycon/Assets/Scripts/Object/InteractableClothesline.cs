94ce074203787f4e67f145c3c23585b09873d15a
GameLab4th_leejungmin <lkswasd@gmail.com>
2025-10-26 00:14:09 +0900

    집을 때 회전 고정,

--- Week02_joycon/Assets/Scripts/Object/InteractableClothesline.cs
+++ Week02_joycon/Assets/Scripts/Object/InteractableClothesline.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class InteractableClothesline : MonoBehaviour, IInteractable
 {
@@ -13,5 +13,18 @@ public class InteractableClothesline : MonoBehaviour, IInteractable
         fullLine.SetActive(true);
 
         InventoryManager.Instance.RemoveAndDestroyItem(ItemName.Bedding);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Contains("Bedding"))
+        {
+            Debug.Log("Clothesline Collision with Bedding");
+            var carryable = collision.gameObject.GetComponent<Carryable>();
+            if (carryable == null) return; // carryable 없으면 종료
+            if (carryable.carrying) return; // 플레이어가 들고 있으면 무시
+            emptyLine.SetActive(false);
+            fullLine.SetActive(true);
+            Destroy(collision.gameObject);
+        }
     }
 }
