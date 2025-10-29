9f780423eb65c05a14de86f1bdf51790ab6ca47b
mingku87 <127922214+mingku87@users.noreply.github.com>
2025-10-30 05:30:56 +0900

    여러분제가좀늦게올거같습니다

--- Week02_joycon/Assets/Scripts/Interaction/FireWood.cs
+++ Week02_joycon/Assets/Scripts/Interaction/FireWood.cs
@@ -1,48 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class FireWood : MonoBehaviour
 {
-    [SerializeField]BoxCollider2D boxCollider2D;
-    float boxCollider2Dx;
-    [SerializeField] GameObject[]Prefabs;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] BoxCollider2D boxCollider2D;
+    [SerializeField] GameObject[] Prefabs;
+
     void Start()
     {
-        if (boxCollider2D==null)
-        {
-            boxCollider2D = GetComponent<BoxCollider2D>();
-        }
-        boxCollider2Dx= boxCollider2D.bounds.size.x;
+        if (boxCollider2D == null) boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision.gameObject.name + "닿음");
         Transform[] childs = collision.transform.GetComponentsInChildren<Transform>();
         foreach (Transform t in childs)
         {
-            if (t.gameObject.CompareTag("Axe"))
+            if (t.gameObject.CompareTag("Axe") && t.GetComponent<Axe>().falling == true)
             {
-                if (t.GetComponent<Axe>().falling == true)//떨어지는 중일 때만
+                for (int i = 0; i < Prefabs.Length; i++)
                 {
-                    for (int i = 0; i < Prefabs.Length; i++)
+                    if (Prefabs[i] != null)
                     {
-                        if (Prefabs[i] != null)
-                        {
-                            Instantiate(Prefabs[i],
-                                new Vector2(transform.position.x/* - boxCollider2Dx / 2 * Prefabs.Length + boxCollider2Dx * i*/, transform.position.y), 
-                                Quaternion.Euler(0,0,transform.localRotation.z));
-                        }
+                        Instantiate(Prefabs[i],
+                            new Vector2(transform.position.x/* - boxCollider2Dx / 2 * Prefabs.Length + boxCollider2Dx * i*/, transform.position.y),
+                            Quaternion.Euler(0, 0, transform.localRotation.z));
                     }
-                    Destroy(gameObject);
                 }
+                Destroy(gameObject);
+
             }
         }
     }
--- Week02_joycon/Assets/Scripts/Interaction/TomatoTree.cs
+++ Week02_joycon/Assets/Scripts/Interaction/TomatoTree.cs
@@ -4,38 +4,26 @@ public class TomatoTree : MonoBehaviour
 {
     [SerializeField] TomatoGrow[] tomatos;
     [SerializeField] int growSpeed = 255;
-    Vector2 firstScale;
     [SerializeField] bool Watering = false;
-
-    void Start()
-    {
-        firstScale = tomatos[0].transform.localScale;
-    }
 
     void Update()
     {
         if (Watering)
         {
-            foreach(var tomato in tomatos)
+            foreach (var tomato in tomatos)
             {
-                if (tomato!=null)
-                tomato.growCurrent += growSpeed * Time.deltaTime;
+                if (tomato != null)
+                    tomato.growCurrent += growSpeed * Time.deltaTime;
             }
-            
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Trigger Entered with: " + collision.gameObject.name);
-        if (collision.transform.CompareTag("Water"))
-            Watering = true;
+        if (collision.transform.CompareTag("Water")) Watering = true;
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        Debug.Log("Trigger Exit with: " + collision.gameObject.name);
-
-        if (collision.CompareTag("Water"))
-            Watering = false;
+        if (collision.CompareTag("Water")) Watering = false;
     }
 }
