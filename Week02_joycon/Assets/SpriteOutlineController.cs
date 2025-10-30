ea82e3665e57f9286d8a5a92d555c518de004bb5
GameLab4th_leejungmin <lkswasd@gmail.com>
2025-10-30 21:10:35 +0900

    아웃라인 기능 추가

---
+++ Week02_joycon/Assets/SpriteOutlineController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteOutlineController : MonoBehaviour
+{
+    public Color outlineColor = Color.black;
+    [Range(0f, 10f)] public float outlineSize = 1f;
+
+    private SpriteRenderer spriteRenderer;
+    private MaterialPropertyBlock propertyBlock;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        propertyBlock = new MaterialPropertyBlock();
+    }
+
+    void LateUpdate()
+    {
+        spriteRenderer.GetPropertyBlock(propertyBlock);
+        propertyBlock.SetColor("_OutlineColor", outlineColor);
+        propertyBlock.SetFloat("_OutlineSize", outlineSize);
+        spriteRenderer.SetPropertyBlock(propertyBlock);
+    }
+}
