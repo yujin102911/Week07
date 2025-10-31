df0eb21c6ac9b64fdb132483b471852a9902d60e
mingku87 <127922214+mingku87@users.noreply.github.com>
2025-10-31 16:23:21 +0900

    약간 수정

--- Week02_joycon/Assets/Scripts/02_Object/00_Common/Carryable.cs
+++ Week02_joycon/Assets/Scripts/02_Object/00_Common/Carryable.cs
@@ -8,11 +8,11 @@ public class Carryable : MonoBehaviour
     [SerializeField] private float gravityScale = 1;
     public float spinAngle = 360;
     private Rigidbody2D _rigidbody;
-    protected LayerMask obstacleMask;
+    protected LayerMask obstacleMask => LayerMask.GetMask(PlayerConstant.ObstacleMask);
     protected bool isCarried;
     public bool throwing = false;
-    [SerializeField, Tooltip("투척 끝나기까지 충돌 가능 횟수")] int throwingCollisionNum = 10;
-    int throwingCollisionCurrent;
+    [SerializeField, Tooltip("투척 끝나기까지 충돌 가능 횟수")] private int throwingCollisionNum = 10;
+    private int throwingCollisionCurrent;
 
     public ItemName GetItemName() => itemName;
     public float GetWeight() => weight;
@@ -27,7 +27,6 @@ public class Carryable : MonoBehaviour
     {
         throwingCollisionCurrent = throwingCollisionNum;
         if (large < 0) large = transform.localScale.x * transform.localScale.y;
-        obstacleMask = LayerMask.GetMask(PlayerConstant.ObstacleMask);
 
         _rigidbody = GetComponent<Rigidbody2D>();
         _rigidbody.gravityScale = weight * gravityScale;
@@ -38,31 +37,23 @@ public class Carryable : MonoBehaviour
 
     private void Update()
     {
-        if (isCarried)
-        {
-            throwing = false;//들고 있으면 투척 상태 해제
-        }
-        if (throwing) //투척 상태이면
-        {
-            transform.Rotate(0f, 0f, -spinAngle * Time.deltaTime);//투척중이면 회전
-
-        }
+        if (isCarried) throwing = false;
+        if (throwing) transform.Rotate(0f, 0f, -spinAngle * Time.deltaTime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (throwing)
         {
-            if (collision.gameObject.layer == LayerMask.NameToLayer("Obstacle")) throwingCollisionCurrent = 0;//장애물과 충돌하면 바로 투척 상태 해제
-            else throwingCollisionCurrent--;//충돌 횟수 감소(플레이어도 충돌 포함)
+            if (collision.gameObject.layer == obstacleMask) throwingCollisionCurrent = 0;
+            else throwingCollisionCurrent--;
+
             if (throwingCollisionCurrent <= 0)
             {
-                throwing = false;//충돌 다하면 투척 상태 해제
-                throwingCollisionCurrent = throwingCollisionNum;//충돌 횟수 초기화
+                throwing = false;
+                throwingCollisionCurrent = throwingCollisionNum;
             }
         }
-
-
     }
 
     private void OnCollisionStay2D(Collision2D collision)
@@ -72,7 +63,6 @@ public class Carryable : MonoBehaviour
 
         SetIsCarried(false);
         GameLogger.Instance.LogDebug(this, $"충돌로 떨어뜨림. 위치 : {transform.position}");
-
     }
 
     private void SetState()
