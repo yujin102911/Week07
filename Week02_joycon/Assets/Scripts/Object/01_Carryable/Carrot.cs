e483c8a476807a3535987900a5c3a846f3d45221
GameLab4th_leejungmin <lkswasd@gmail.com>
2025-10-31 21:55:27 +0900

    수프 수정

---
+++ Week02_joycon/Assets/Scripts/Object/01_Carryable/Carrot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Carrot : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+    
+    }
+        private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Transform[] childs = collision.transform.GetComponentsInChildren<Transform>();
+        foreach (Transform t in childs)
+        {
+            if (!t.TryGetComponent<Axe>(out Axe axe)) continue;
+            if (axe.falling == true || axe.throwing)
+            {
+                GameObject carrot = Instantiate(new GameObject(), transform.position,Quaternion.identity);
+                carrot.name = "carrots";
+                carrot.layer = LayerMask.NameToLayer("Carryable");
+                carrot.transform.SetParent(transform);
+                carrot.AddComponent<Carrot>();
+            }
+        }
+    }
+}
---
+++ Week02_joycon/Assets/Scripts/Object/01_Carryable/Carrots.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Carrots : MonoBehaviour
+{
+    [SerializeField] public List<GameObject> carrotPrefabs;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
