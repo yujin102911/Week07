8b050c0529044961ce00c3304af9ce9c006c12b6
GameLab4th_leejungmin <lkswasd@gmail.com>
2025-11-01 10:26:37 +0900

    펌프 제작중

---
+++ Week02_joycon/Assets/Scripts/02_Object/02_Interactable/InteractablePump.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class InteractablePump : MonoBehaviour
+{
+    [SerializeField] Controller2D controller2D;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (controller2D == null)
+            controller2D = Player.Instance.GetComponent<Controller2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
